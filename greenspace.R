201dedf7801736c0536f0ba8367ea2e76413d974
amandahao <amandahao@gmail.com>
2024-03-15 14:36:49 -0400

    new greenspace functions

--- greenspace.R
+++ greenspace.R
@@ -1,57 +1,28 @@
 # library(devtools)
 # devtools::install_github("sachit27/greenR", dependencies = TRUE)
 library(greenR)
+library(dplyr)
+library(leaflet)
 
-data <- get_osm_data("Washington, D.C., United States")
-green_areas_data <- data$green_areas
-visualize_green_spaces(green_areas_data)
+source("get_green_data.R")
+source("modified_visualizer.R")
 
-## try other cities; LA, SF, Chicago, Houston
+## combine virginia counties
 
-# data <- get_osm_data("")
-# green_areas_data <- data$green_areas
-# visualize_green_spaces(green_areas_data)
+virginia_green_data <- get_green_data("Washington, D.C., United States")$osm_polygons
 
-# make a function  that gets data of minimum amount of cities to load green space
-# data for entirety of US, append all data in df; use visualize_green_spaces function
-# on github and add it to server.R / ui.R; everything else in global.R
+locations <- c("Arlington County, Virginia, United States",
+               "Fairfax County, Virginia, United States",
+               "Loudoun County, Virginia, United States")
 
-cities <- c("Los Angeles, California, United States",
-            "Chicago, Illinois, United States",
-            "Houston, Texas, United States",
-            # "Phoenix, Arizona, United States",
-            # "Philadelphia, Pennsylvania, United States",
-            # "San Antonio, Texas, United States",
-            # "San Diego, California, United States",
-            # "Dallas, Texas, United States",
-            # "San Jose, California, United States",
-            # "Austin, Texas, United States",
-            # "Jacksonville, Florida, United States",
-            "San Francisco, California, United States",
-            # "Columbus, Ohio, United States",
-            # "Indianapolis, Indiana, United States",
-            # "Fort Worth, Texas, United States",
-            # "Charlotte, North Carolina, United States",
-            # "Seattle, Washington, United States",
-            # "Denver, Colorado, United States",
-            "Washington, D.C., United States")
-
-# Initialize an empty list to store green space data
-all_green_data <- list()
-
-# Iterate over cities to get green space data for each
-for (city in cities) {
-  data <- get_osm_data(city)
-  green_areas_data <- data$green_areas
-  all_green_data <- append(all_green_data, list(green_areas_data))
+for (location in locations) {
+  green_data <- get_green_data(location)
+  virginia_green_data <- bind_rows(virginia_green_data, green_data$osm_polygons)
 }
 
-# Combine all the green space data into one variable
-combined_green_data <- do.call(rbind, all_green_data)
+modified_visualizer(virginia_green_data)
 
-# Visualize the combined green space data
-visualize_green_spaces(combined_green_data)
-
+# test <- get_green_data("")$osm_polygons
 
 ## extra greenR functions
 
@@ -63,4 +34,3 @@ visualize_green_spaces(combined_green_data)
 # 
 # map <- plot_green_index(green_index, interactive = TRUE, base_map = "CartoDB.DarkMatter")
 # print(map)
->>>>>>> Stashed changes
